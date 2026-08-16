ed9b3c66dd327f935205d49458eca893343105c4
hedgebirdcat <hedge.bird.cat.0410@gmail.com>
2026-08-16 18:12:44 +0900

    Update import paths for save.js and gameFirebase.js

--- friends.js
+++ friends.js
@@ -8,7 +8,7 @@
 // フレンドのプロフィール表示は friend-profile-screen(独立画面、
 // screenNav.js が管理)を使う。
 
-import { getPlayerData } from "./save.js";
+import { getPlayerData } from "./save.js?v=2";
 
 import {
     findByFriendCode,
@@ -17,7 +17,7 @@ import {
     getIncomingRequests,
     getFriends,
     getFriendPublicData
-} from "./gameFirebase.js";
+} from "./gameFirebase.js?v=2";
 
 let els = {};
 
